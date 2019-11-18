909081b2cf698fb0e04bbc16799fd941f70a0dd7
MinjaeLyou <lminjae0313@gmail.com>
2019-11-18 20:46:25 +0900

    [ADD] New user, travle, user_travel table

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var sequelize = require('./models').sequelize;
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -37,5 +39,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+sequelize.sync()
 
 module.exports = app;
