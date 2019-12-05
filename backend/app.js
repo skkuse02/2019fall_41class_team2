723dad3f99dd9ebbeb41d0f8328345adfb42ed02
MinjaeLyou <lminjae0313@gmail.com>
2019-12-06 01:21:52 +0900

    [ADD] Routing url

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/authentication');
+var travelRouter = require('./routes/travel');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,6 +42,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-// sequelize.sync()
+ //sequelize.sync()
 
 module.exports = app;
