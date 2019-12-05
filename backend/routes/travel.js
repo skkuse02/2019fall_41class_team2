f95da3cceb756fd46d40b7568b6d24fe81f9766d
MinjaeLyou <lminjae0313@gmail.com>
2019-12-06 01:20:08 +0900

    [ADD] New function to get nation list

--- backend/routes/travel.js
+++ backend/routes/travel.js
@@ -12,7 +12,8 @@ router.post('/addTravel', wrap(async (req, res) => {
     category: req.body.category,
     total_budget: req.body.total_budget,
     start_date: req.body.start_date,
-    end_date: req.body.end_date
+    end_date: req.body.end_date,
+    nation_id: parseInt(req.body.country)
   });
   if(travel) {
     res.send({
@@ -36,6 +37,23 @@ router.post('/addNation', wrap(async (req, res) => {
       name: cities[i].country,
       latitude: cities[i].latitude,
       longitude: cities[i].longitude,
+    });
+  }
+}))
+
+router.get('/getNation', wrap(async (req, res) => {
+  const nation = await models.Nation.findAll({
+    attributes: ['nation_id', 'name']
+  });
+  console.log(nation)
+  if(nation) {
+    res.send({
+      result: nation,
+      data: nation
+    });
+  } else {
+    res.send({
+      result: false
     });
   }
 }))
