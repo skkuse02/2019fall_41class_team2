e38b881f5d403e0833784c35296720e6b4d26a7f
tae6ae <49182823+tae6ae@users.noreply.github.com>
2019-12-08 06:07:33 +0900

    [UPDATE] Explore page to list schedules

--- frontend/screens/Explore.js
+++ frontend/screens/Explore.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import { Animated, Dimensions, Image, StyleSheet, ScrollView, TouchableOpacity } from 'react-native'
-import Icon from 'react-native-vector-icons';
+import { FlatList, Dimensions, Image, StyleSheet, ScrollView, TouchableOpacity } from 'react-native'
 import { LinearGradient } from 'expo';
 
 import { Button, Input, Block, Text } from '../components';
@@ -9,122 +8,70 @@ import { theme, mocks } from '../constants';
 const { width, height } = Dimensions.get('window');
 
 class Explore extends Component {
-  state = { 
-    searchFocus: new Animated.Value(0.6),
-    searchString: null,
+  state = {
+    refreshing: false,
+    loading: false,
+    travel_id: '',
+    data: [],
+  };
+  
+  async getSchedule() {
+    let url = 'http://6eaec575.ngrok.io/schedule/getSchedule';
+    this.setState({ loading: true });
+    const { travel_id } = this.state;
+    let options = {
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                  
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json;charset=UTF-8'
+                },
+                body: JSON.stringify({
+                  travel_id: travel_id
+                })
+            };
+    let response = await fetch(url, options);
+    
+    let responseOK = response && response.ok;
+    let nation = []
+    if (responseOK){
+      let resJson = await response.json()
+      let data = resJson.data
+      console.log(response)
+      console.log(data[0])
+      this.setState({data: data, loading: false})
+    }
   }
 
-  handleSearchFocus(status) {
-    Animated.timing(
-      this.state.searchFocus,
-      {
-        toValue: status ? 0.8 : 0.6, // status === true, increase flex size
-        duration: 150, // ms
-      }
-    ).start();
-  }
-
-  async componentDidMount(){
-    const { navigation } = this.props;
-    const browse = navigation.getParam('browse', 'no Browse data');
-    console.log(browse)
-  }
-
-  renderSearch() {
-    const { searchString, searchFocus } = this.state;
-    const isEditing = searchFocus && searchString;
-
-    return (
-      <Block animated middle flex={searchFocus} style={styles.search}>
-        <Input
-          placeholder="Search"
-          placeholderTextColor={theme.colors.gray2}
-          style={styles.searchInput}
-          onFocus={() => this.handleSearchFocus(true)}
-          onBlur={() => this.handleSearchFocus(false)}
-          onChangeText={text => this.setState({ searchString: text })}
-          value={searchString}
-          onRightPress={() => isEditing ? this.setState({ searchString: null }) : null}
-          rightStyle={styles.searchRight}
-          
-        />
-      </Block>
-    )
-  }
-
-  renderImage(img, index) {
-    const { navigation } = this.props;
-    const sizes = Image.resolveAssetSource(img);
-    const fullWidth = width - (theme.sizes.padding * 2.5);
-    const resize = (sizes.width * 100) / fullWidth;
-    const imgWidth = resize > 75 ? fullWidth : sizes.width * 1;
-
-    return (
-      <TouchableOpacity
-        key={`img-${index}`}
-        onPress={() => navigation.navigate('Product')}
-      >
-        <Image
-          source={img}
-          style={[
-            styles.image,
-            { minWidth: imgWidth, maxWidth: imgWidth }
-          ]}
-        />
-      </TouchableOpacity>
-    )
-  }
-
-  renderExplore() {
-    const { images, navigation } = this.props;
-    const mainImage = images[0];
-
-    return (
-      <Block style={{ marginBottom: height / 3 }}>
-        <TouchableOpacity
-          style={[ styles.image, styles.mainImage ]}
-          onPress={() => navigation.navigate('Product')}
-        >
-          <Image source={mainImage} style={[styles.image, styles.mainImage]} />
-        </TouchableOpacity>
-        <Block row space="between" wrap>
-          {
-            images.slice(1).map((img, index) => this.renderImage(img, index))
-          }
-        </Block>
-      </Block>
-    )
-  }
-
-  renderFooter() {
-    return (
-      <LinearGradient
-        locations={[0.5, 1]}
-        style={styles.footer}
-        colors={['rgba(255,255,255,0)', 'rgba(255,255,255,0.6)']}
-      >
-        <Button gradient style={{ width: width / 2.678 }}>
-          <Text bold white center>Filter</Text>
-        </Button>
-      </LinearGradient>
-    )
+  onRefresh = () => {
+    this.getSchedule();
   }
 
   render() {
     const { navigation } = this.props;
     const browse = navigation.getParam('browse', 'no Browse data');
+    this.setState({ travel_id: browse });
     return (
       <Block>
         <Block flex={false} row center space="between" style={styles.header}>
           <Text h1 bold>{browse.title}</Text>
-          {this.renderSearch()}
         </Block>
-
-        <ScrollView showsVerticalScrollIndicator={false} style={styles.explore}>
-          {this.renderExplore()}
-        </ScrollView>
-
-        {this.renderFooter()}
+          <FlatList
+            data={this.state.data}
+            initialNumToRender={20}
+            refreshing={this.state.refreshing}
+            onRefresh={this.onRefresh}
+            renderItem={({ item }) => {
+              return (
+                <ListItem
+                  roundAvatar
+                  avatar={{uri: item.avatar}}
+                  title={item.name}
+                />
+              );
+            }}
+          />       
       </Block>
     )
   }
