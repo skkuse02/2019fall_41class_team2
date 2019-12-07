1f389dead659a8263a7b832ecfedea2967eea409
MinjaeLyou <lminjae0313@gmail.com>
2019-12-07 23:46:10 +0900

    [ADD] Flag in browse page

--- frontend/screens/Browse.js
+++ frontend/screens/Browse.js
@@ -37,37 +37,58 @@ class Browse extends Component {
     if (responseOK){
       let resJson = await response.json()
       let data = resJson.data
-      console.log(response)
+      //console.log(response)
       console.log(data)
       for(i = 0; i < data.length ; i++){
+        console.log(data[i].Nation.name.toLowerCase())
+        let url = `https://restcountries.eu/rest/v2/name/${data[i].Nation.name.toLowerCase()}`;
+        let options = {
+                    method: 'GET',
+                    mode: 'cors',
+                    headers: {
+                      
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json;charset=UTF-8',
+                        'fullText': true
+                    }
+                };
+        let res = await fetch(url, options);
+        let resJ = await res.json()
+        let code = resJ[0].alpha2Code.toLowerCase()
+        console.log(code)
+        console.log(`https://www.countryflags.io/${code}/flat/24.png`)
         let travel = {}
         if(data[i].category == 'fri'){
           travel = {
             id: data[i].title,
             name: data[i].title,
             tags: ['Friend'],
-            content: data[i].content
+            content: data[i].content,
+            img: `https://www.countryflags.io/${code}/flat/64.png`
           }
         } else if (data[i].category == 'fam'){
           travel = {
             id: data[i].title,
             name: data[i].title,
             tags: ['Family'],
-            content: data[i].content
+            content: data[i].content,
+            img: `https://www.countryflags.io/${code}/flat/64.png`
           }
         } else if (data[i].category == 'alo'){
           travel = {
             id: data[i].title,
             name: data[i].title,
             tags: ['Alone'],
-            content: data[i].content
+            content: data[i].content,
+            img: `https://www.countryflags.io/${code}/flat/64.png`
           }
         } else{
           travel = {
             id: data[i].title,
             name: data[i].title,
             tags: ['Couple'],
-            content: data[i].content
+            content: data[i].content,
+            img: `https://www.countryflags.io/${code}/flat/64.png`
           }
         }
         
@@ -151,11 +172,13 @@ class Browse extends Component {
                 onPress={() => navigation.navigate('Explore', { category })}
               >
                 <Card center middle shadow style={styles.category}>
-                  <Badge margin={[0, 0, 15]} size={50} color="rgba(41,216,143,0.20)">
-                    <Image source={now_each.image} />
+                  <Badge margin={[0, 0, 15]} size={60} color="rgba(41,216,143,0.20)">
+                    
+                    <Image source={{uri: now_each.img}}
+       style={{width: 40, height: 40}} />
                   </Badge>
                   <Text medium height={20}>{now_each.name}</Text>
-                  <Text gray caption>{now_each.content} products</Text>
+                  <Text gray caption>{now_each.content}</Text>
                 </Card>
               </TouchableOpacity>
             ))}
